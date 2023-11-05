7d672e7f58398d9e016089b546c54d60e565a325
Tiffany Prosser <tiffany.prosser@deptagency.com>
2023-11-05 01:13:49 +0000

    // Added configuration

--- src/NetC.JuniorDeveloperExam.Web/App_Start/RouteConfig.cs
+++ src/NetC.JuniorDeveloperExam.Web/App_Start/RouteConfig.cs
@@ -8,6 +8,24 @@ namespace NetC.JuniorDeveloperExam.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Blog",
+                url: "",
+                defaults: new { controller = "Blog", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "BlogPostById",
+                url: "blog/show/{id}",
+                defaults: new { controller = "Blog", action = "GetBlogPostsById", title = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
--- src/NetC.JuniorDeveloperExam.Web/Global.asax.cs
+++ src/NetC.JuniorDeveloperExam.Web/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System.Web;
+﻿using NetC.JuniorDeveloperExam.Web.Services;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Services.Description;
+using Unity;
+using Unity.AspNet.Mvc; // Import the Unity.Mvc5 namespace
 
 namespace NetC.JuniorDeveloperExam.Web
 {
@@ -8,9 +13,16 @@ namespace NetC.JuniorDeveloperExam.Web
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var container = new UnityContainer();
+            container.RegisterType<BlogPostService>();
+
+            // Set the Unity dependency resolver to use the Unity container
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
 }
